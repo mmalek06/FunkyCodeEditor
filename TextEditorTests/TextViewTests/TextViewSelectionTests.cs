776a35ea8e9764061fd0288787cedfeb86ca92f1
marek <mmalek06@gmail.com>
2016-02-03 20:54:27 +0100

    adding text selection tests

--- TextEditorTests/TextViewTests/TextViewSelectionTests.cs
+++ TextEditorTests/TextViewTests/TextViewSelectionTests.cs
@@ -98,6 +98,66 @@ namespace TextEditorTests.TextViewTests {
             Assert.AreEqual(text2, selectedParts.First());
             Assert.AreEqual(text3, selectedParts.Last());
         }
-                
+              
+        [TestMethod]
+        public void SelectTwoOfFourLinesBottomToTop_CursorPositionIsAtTheEndOfText1() {
+            string text1 = "shop";
+            string text2 = "Where she sits she shines, and where she shines she sits.";
+            var tv = new TextEditor.Views.TextView.View();
+            var textInfo = new TextEditor.Views.TextView.TextInfo(tv);
+            var sv = new TextEditor.Views.SelectionView.View(textInfo);
+            var cv = new TextEditor.Views.CaretView.View(textInfo);
+            var startingPosition = new TextEditor.DataStructures.TextPosition {
+                Line = 1,
+                Column = text2.Length
+            };
+            var endingPosition = new TextEditor.DataStructures.TextPosition {
+                Line = 0,
+                Column = text1.Length
+            };
+
+            cv.CaretMoved += tv.HandleCaretMove;
+
+            tv.EnterText(text1);
+            tv.EnterText("\r");
+            tv.EnterText(text2);
+            sv.Select(startingPosition);
+            sv.Select(endingPosition);
+            cv.MoveCursor(endingPosition);
+
+            Assert.AreEqual(0, tv.ActivePosition.Line);
+            Assert.AreEqual(text1.Length, tv.ActivePosition.Column);
+        }
+
+        [TestMethod]
+        public void SelectTwoOfFourLinesTopToBottom_CursorPositionIsAtTheEndOfText2() {
+            string text1 = "Where she sits she shines, and where she shines she sits.";
+            string text2 = "shop";
+            var tv = new TextEditor.Views.TextView.View();
+            var textInfo = new TextEditor.Views.TextView.TextInfo(tv);
+            var sv = new TextEditor.Views.SelectionView.View(textInfo);
+            var cv = new TextEditor.Views.CaretView.View(textInfo);
+            var startingPosition = new TextEditor.DataStructures.TextPosition {
+                Line = 0,
+                Column = text1.Length
+            };
+            var endingPosition = new TextEditor.DataStructures.TextPosition {
+                Line = 1,
+                Column = text2.Length
+            };
+
+            cv.CaretMoved += tv.HandleCaretMove;
+
+            tv.EnterText(text1);
+            tv.EnterText("\r");
+            tv.EnterText(text2);
+            sv.Select(startingPosition);
+            sv.Select(endingPosition);
+            cv.MoveCursor(endingPosition);
+
+            Assert.AreEqual(1, tv.ActivePosition.Line);
+            Assert.AreEqual(text2.Length, tv.ActivePosition.Column);
+        }
+
     }
 }
