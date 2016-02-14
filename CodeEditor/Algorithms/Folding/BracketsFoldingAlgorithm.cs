80fe9fb142fa3b08166eefa83e35554b3599477d
marek <mmalek06@gmail.com>
2016-02-14 13:11:38 +0100

    fixed variable names

--- CodeEditor/Algorithms/Folding/BracketsFoldingAlgorithm.cs
+++ CodeEditor/Algorithms/Folding/BracketsFoldingAlgorithm.cs
@@ -36,15 +36,15 @@ namespace CodeEditor.Algorithms.Folding {
             var linesArray = textLines.ToArray();
             int linesCount = textLines.Count();
 
-            for (int i = linesCount - 1; i >= startPosition.Line; i--) {
-                for (int j = linesArray[i].Length - 1; j >= 0; j--) {
-                    char character = linesArray[i][j];
+            for (int lineIndex = linesCount - 1; lineIndex >= startPosition.Line; lineIndex--) {
+                for (int columnIndex = linesArray[lineIndex].Length - 1; columnIndex >= 0; columnIndex--) {
+                    char character = linesArray[lineIndex][columnIndex];
 
                     if (character == '}') {
                         closingBracketNum++;
                     }
                     if (closingBracketNum == openingBracketNum && character == '}') {
-                        return new TextPosition { Column = j + 1, Line = i };
+                        return new TextPosition { Column = columnIndex + 1, Line = lineIndex };
                     }
                 }
             }
