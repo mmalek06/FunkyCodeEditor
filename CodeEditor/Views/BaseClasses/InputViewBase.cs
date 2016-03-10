e797aaf103541ed154e6ae21bfcfe89236b4a25b
marek <mmalek06@gmail.com>
2016-03-10 20:13:46 +0100

    final commit before branching

---
+++ CodeEditor/Views/BaseClasses/InputViewBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace CodeEditor.Views.BaseClasses {
+    internal abstract class InputViewBase : ViewBase {
+        
+        #region constructor
+
+        public InputViewBase() : base() {
+            Focusable = true;
+        }
+
+        #endregion
+
+        #region methods
+
+        protected override void OnRender(DrawingContext drawingContext) => 
+            drawingContext.DrawRectangle(Brushes.Transparent, null, new Rect(0, 0, Width, Height));
+
+        #endregion
+
+    }
+}
